c9d1a3ccfec53d1fbd78e1be204c97fe9d8e3e78
Petr Bouchal <pbouchal@gmail.com>
2015-08-04 17:41:01 +0200

    platform flexible setup script

--- d15eval/d15eval_setup.R
+++ d15eval/d15eval_setup.R
@@ -1,14 +1,19 @@
-# Sys.setlocale(category = "LC_ALL", locale = "Czech_Czech Republic.1252")
-Sys.setlocale(category = "LC_ALL", locale = "cs_CZ.UTF-8")
-
 library(pbtools)
 library(dplyr)
 library(reshape2)
 library(tidyr)
 library(Cairo)
 
-loc <- "C:/Users/boupet/Downloads/eval"
-loc <- "~/Documents/Work/Discover/Discover 2015/Evaluace/data_final/"
+if(whatplatform()=="win") {
+  Sys.setlocale(category = "LC_ALL", locale = "Czech_Czech Republic.1252")
+  loc <- "C:/Users/boupet/Downloads/eval"
+  options(encoding = "UTF8")
+  } else {
+  Sys.setlocale(category = "LC_ALL", locale = "cs_CZ.UTF-8")
+  loc <- "~/Documents/Work/Discover/Discover 2015/Evaluace/data_final/"
+  }
+
+
 
 ## Load data using LimeSurvey script ######
 
