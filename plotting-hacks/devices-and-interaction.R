5190836950b24811e48fc8b457c90582b5810855
Petr Bouchal <pbouchal@gmail.com>
2016-03-08 18:25:16 +0100

    add sample plotly / ggplotly code

--- plotting-hacks/devices-and-interaction.R
+++ plotting-hacks/devices-and-interaction.R
@@ -4,8 +4,12 @@ library(ggiraph)
 library(lubridate)
 library(dplyr)
 library(Cairo)
+library(gganimate)
+library(animation)
+library(ggiraph)
 
-v <- read_excel("~/Dropbox/MMR/Admin/Výplatní pásky/vyplaty_data.xlsx")
+# v <- read_excel("~/Dropbox/MMR/Admin/Výplatní pásky/vyplaty_data.xlsx")
+v <- read_excel("D:/usr/Dropbox/MMR/Admin/Výplatní pásky/vyplaty_data.xlsx")
 v <- v %>%
   mutate(date=as.POSIXct(strptime(paste(year,month,"01",sep="-"), format="%Y-%m-%d")),
          tooltip=paste(month.name[month],year),
@@ -18,10 +22,13 @@ gg_point_1 <- ggplot(v, aes(x = date, y = net, tooltip = tooltip, group=grp) ) +
   geom_line() +
   geom_point_interactive(size=3)
 
+library(plotly)
+ggplotly(gg_point_1)
+
+print(gg_point_1)
+
 ggiraph(code = {print(gg_point_1 + mytheme_main)}, width = 7, height = 6)
 
-library(gganimate)
-library(animation)
 
 gg_point_2 <- ggplot(v, aes(x = date, y = net, frame = date, group=grp)) + 
   geom_line(aes(group=grp, cumulative=T)) +
