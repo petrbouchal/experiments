90f69342fb253eb24751ff3b81fd2f6072de1e45
petrbouchal <pbouchal@gmail.com>
2016-03-14 22:34:27 +0100

    violin test

--- violintest.R
+++ violintest.R
@@ -3,6 +3,8 @@ vd <- read.csv("violintest.tsv")
 library(ggplot2)
 
 ggplot(vd, aes(kurz,znamka, group=kurz)) + 
-  geom_violin() +
+  geom_violin(colour=NA, aes(fill=kurz)) +
   geom_point(position = position_jitter(width=0)) +
   facet_wrap(~ turnus)
+
+# two-sided asymmetrical violin?
