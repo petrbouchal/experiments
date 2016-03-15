a7a4f42d547ff02d87af77b4fa09714253eb3373
petrbouchal <pbouchal@gmail.com>
2016-03-15 22:39:57 +0100

    subtitle ggplot2 code

--- plotting-hacks/ggplot-interact-animate.R
+++ plotting-hacks/ggplot-interact-animate.R
@@ -28,3 +28,7 @@ gg_point_2 <- ggplot(dataset, aes(x = disp, y = qsec,
 ggiraph(code = {print(gg_point_2 + mytheme_main)}, 
         width = 7, height = 6, 
         hover_css = "fill:orange;r:6px;")
+
+# with subtitle, depends on https://github.com/hadley/ggplot2/pull/1582
+ggiraph(code={print(ggplot(data=mtcars, aes(mpg, cyl)) + geom_point_interactive() + labs(title="blah", subtitle="blah blah", annotation="blahnotation"))})
+
